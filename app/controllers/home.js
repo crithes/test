bf0c838aa79d808015d635a31189e583ddd5bd68
crithes <3540159594@qq.com>
2019-06-16 16:08:20 +0800

    second commit

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -12,7 +12,8 @@ router.get('/', (req, res, next) => {
     if (err) return next(err);
     res.render('index', {
       title: 'Generator-Express MVC',
-      articles: articles
+      articles: articles,
+      pretty:true
     });
   });
 });
